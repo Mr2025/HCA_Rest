dbdf2c987674d1fc7639cf32d6fff090b6f2fa2b
Jmac <jmac@quotedcar.com>
2019-05-06 21:31:59 -0700

    Added WayPoints

--- public/routes/flag.js
+++ public/routes/flag.js
@@ -47,7 +47,7 @@ class Flag{
                 }
                 res.status(200).json({success:'success'});
                 break;
-            case 'waypoint':
+            case 'waypoint': 
             case 'waypoints':
                     log(`Flag:processOwn Ignoring Message type:[${chalk.blue(type)}]`);
                     res.status(200).json({success:'success'});
